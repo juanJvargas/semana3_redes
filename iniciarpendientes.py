13cf258d84798866b03ac4cdb9f0746dedfda28e
juanjvargas <juan.jose.vargas@correounivalle.edu.co>
2017-11-19 21:28:13 -0500

    Add files via upload

--- iniciarpendientes.py
+++ iniciarpendientes.py
@@ -11,4 +11,14 @@ size=len(descargar)-1
 aux=0
 while aux!=size:
 	tc.add_torrent(descargar[aux])
+	last= len(tc.get_torrents())
+	torrent=tc.get_torrent(last)
+	name= torrent.name
+	magnetlink=descargar[aux]
+	magnetlink=magnetlink.replace("?","$")
+	magnetlink=magnetlink.replace(".","^")
+	magnetlink=magnetlink.replace("%","|")
+	name=name.replace(" ","=")
+	url="https://proyectoredestorresvargas.herokuapp.com/actualizar"+ magnetlink +","+name
+	response=requests.get(url)
 	aux =aux+1
