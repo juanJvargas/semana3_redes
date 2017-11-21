274a083029ecac818c1e72703c2b88da80c96e18
juanjvargas <juan.jose.vargas@correounivalle.edu.co>
2017-11-20 22:25:05 -0500

    Add files via upload

--- iniciarpendientes.py
+++ iniciarpendientes.py
@@ -1,6 +1,7 @@
 import requests
 import transmissionrpc
 
+
 tc = transmissionrpc.Client('localhost', port=9091)
 
 url="https://proyectoredestorresvargas.herokuapp.com/pendiente"
@@ -11,14 +12,19 @@ size=len(descargar)-1
 aux=0
 while aux!=size:
 	tc.add_torrent(descargar[aux])
-	last= len(tc.get_torrents())
-	torrent=tc.get_torrent(last)
-	name= torrent.name
+	torrents =tc.get_torrents()
+	tama= len(torrents)
+	name= str(torrents[tama-1].id)
 	magnetlink=descargar[aux]
 	magnetlink=magnetlink.replace("?","$")
 	magnetlink=magnetlink.replace(".","^")
 	magnetlink=magnetlink.replace("%","|")
-	name=name.replace(" ","=")
-	url="https://proyectoredestorresvargas.herokuapp.com/actualizar"+ magnetlink +","+name
+	data=""+magnetlink +","+name
+	print(name)
+	url="https://proyectoredestorresvargas.herokuapp.com/actualizar/"+ data
 	response=requests.get(url)
+	print(response.status_code)
 	aux =aux+1
+	
+
+
--- mandardatos.py
+++ mandardatos.py
@@ -8,12 +8,9 @@ print(size)
 aux=0
 while aux!=size:
 	magnetlink="";
-	magnetlink=torrents[aux].magnetLink
-	magnetlink=magnetlink.replace("?","$")
-	magnetlink=magnetlink.replace(".","^")
-	magnetlink=magnetlink.replace("%","|")
+	name=str(torrents[aux].id)
 	progreso=torrents[aux].progress
-	url="https://proyectoredestorresvargas.herokuapp.com/actualizar/"+magnetlink+","+str(progreso)
+	url="https://proyectoredestorresvargas.herokuapp.com/actualizar/"+name+", progreso,"+str(progreso)
 	response=requests.get(url)
 	print(response.content)
 	aux=aux+1
